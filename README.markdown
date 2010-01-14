624832e8f018016bcf6cc24df06d861dbd5626cc
Stuart Halloway <stu@thinkrelevance.com>
2010-01-14 05:53:58 -0500

    working first lab

---
+++ README.markdown
@@ -0,0 +1,17 @@
+# labrepl for Clojure
+
+Copyright (c) Relevance, Inc. All rights reserved.
+
+# License
+
+The use and distribution terms for this software are covered by the Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html at the root of this distribution. By using this software in any fashion, you are agreeing to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
+
+# Getting Started
+
+* Make sure you have Java installed.
+* Make sure you have leiningen installed (http://github.com/technomancy/leiningen).
+* Run `lein deps` to install dependent libs.
+* Run `script/console` to launch the labrepl.
+* Browse to localhost:8080 for instructions.
+ 
+ 
--- src/labs/intro.clj
+++ src/labs/intro.clj
@@ -1,7 +1,128 @@
-(ns labs.intro)
+(ns labs.intro
+  (:use clojure.contrib.pprint))
+
+(defmacro code
+  [code]
+  `[:pre [:code (with-out-str (pprint '~code))]])
+
+(defn overview
+  []
+  [[:h3 "Overview"]
+   [:p "In this lab you will learn to call functions from the Clojure Read-Eval-Print Loop (REPL), and a set of tools that you can use from the REPL to explore Clojure. Note that some of these tools are not available with a minimal install of Clojure. Later in the course you will see how to include all of these tools in your own projects."]
+   [:p "You need to be running the labrepl to perform the steps in this lab."]])
+
+(defn functions
+  []
+  [[:h3 "Functions"]
+   [:ol
+    [:li "Find your labrepl window, which should have a prompt like "
+     [:pre [:code "user=> "]]
+     "This prompt tells you that you are currently working in the user namespace, which is a scratch namespace for experimenting. From the prompt, you can call functions with forms like this:"
+     [:pre [:code "(funcname arg1 arg2 ...)"]]
+     [:li 
+      "Go ahead and try \"hello world.\""
+      [:pre [:code "(println \"hello world\")"]]]
+     [:li
+      "Function names do not have to be words. Try some math functions:"
+      (code (+ 1 2))
+      (code (/ 22 7))
+      (code (/ 22.0 7))
+      (code (+ 1 2 3 4 5))]
+     [:li
+      "You can define your own functions with "
+      [:code "defn"]
+      "Use the following form to define a function named double that doubles its argument."
+      (code (defn double
+               [arg]
+               (* 2 arg)))]
+     [:li
+      "Try defining and calling a few other functions of your choosing."]]]])
+
+(defn documentation
+  []
+  [[:h3 "Documentation"]
+   [:ol
+    [:li
+     "To learn about a function, you can examine its docstring. Print the docstring for "
+     [:code "println"]
+     "using the form"
+     (code (doc println))]
+    [:li
+     "If you are not sure of the name of a function, you can search for docs with "
+     [:code "find-doc"]
+     ". This can sometimes return a huge amount of information!"
+     "Try "
+     (code (find-doc "append"))]
+    [:li
+     [:code "find-doc"]
+     " can also handle regular expressions, which in Clojure look like strings prepended by an octothorpe. Use a regular expression to find all the " [:code "find-"] " functions."
+     (code (find-doc #"find-\w+"))]
+    [:li
+     "There is no documentation like the source code. Use "
+     [:code "source"]
+     " to view the soure code of a function."
+     (code (source println))]
+    [:li
+     "Notice how the source for "
+     [:code "println"]
+     " includes a string before the arguments. This is how  you define docstrings. Try it with your own functions: "
+     (code (defn double
+             "Doubles arg. Don't write redundant docstrings like this in real code."
+             (* 2 arg)))
+     (code (doc double))]]])
+
+(defn java
+  []
+  [[:h3 "Java"]
+   [:ol
+    [:li
+     "Clojure provides direct, unproxied access to Java. To call a static function use a form like "
+     (code (ClassName/function & args))
+     "(The "
+     [:code "& args"]
+     " is shorthand for zero or more arguments.) Try calling"
+     (code (System/getProperty "java.home"))]
+    [:li
+     "You can create Java instances with forms like "
+     (code (new ClassName & args))
+     " or the terser, more idiomatic "
+     (code (ClassName. & args))
+     " Try making a date. "
+     (code (java.util.Date.))]
+    [:li
+     "Once you have an instance you probably will want to call methods on it. For now, stuff your date into a global using "
+     [:code "def"] ": "
+     (code (def now (java.util.Date.)))]
+    [:li
+     "You can call instance methods like so:"
+     (code (.getMonth now))
+     (code (.toGMTString now))]
+    [:li
+     "If you can't remember all the date methods off the top of your head, you can pass either a class or an instance to "
+     [:code "javadoc"]
+     (code (javadoc now))
+     (code (javadoc "java.util.Date"))]
+    [:li
+     "Or, to simply list method signatures, use show."
+     (code (show now))
+     (code (show "java.util.Date"))]]])
+
+(defn bonus
+  []
+  [[:h3 "Bonus"]
+   "What do you think the following forms will do?"
+   (code (doc doc))
+   (code (find-doc "find-doc"))
+   (code (source source))
+   (code (show show))
+   (code (javadoc javadoc))
+   "Try them. Do you understand what you are seeing?"])
 
 (defn instructions
   []
-  [:h2 "Welcome to Clojure"
-   [:h3 "Objectives"]
-   [:h3 "Steps"]])
+  (concat
+   (overview)
+   (functions)
+   (documentation)
+   (java)
+   (bonus)))
