4fac9b245d3e79a918b6471413ca3f30af4d5f63
Stuart Halloway <stu@thinkrelevance.com>
2010-02-01 10:40:27 -0500

    accounts-2

--- test/solutions/accounts_2_test.clj
+++ test/solutions/accounts_2_test.clj
@@ -1,5 +1,5 @@
-(ns solutions.accounts-1-test
-  (:use circumspec solutions.accounts-1))
+(ns solutions.accounts-2-test
+  (:use circumspec solutions.accounts-2))
 
 (testing "total balance is maintined when running in isolation"
   (let [accounts (make-accounts {:initial-balance 100
@@ -8,11 +8,8 @@
     (dotimes [_ 10] (random-transaction accounts))
     (should (= 1000 (total-balance accounts)) "balance after")))
 
-;; statistically this could fail
-(testing "total balance is *not* maintained across threads"
+(testing "total balance is maintained across threads"
   (let [accounts (make-accounts {:initial-balance 100
-                                 :count 10})]
-    (should (throws?
-             AssertionError
-             #"Assert failed: \(= expected-balance \(total-balance accounts\)\)"
-             (trial)))))
+                                 :count 10})
+        trial-results (trial)]
+    (should (= 1000 (total-balance (:accounts trial-results))))))
