f27f97536f6e584c284b898ad4abd732c424684e
Shawn Hoover <shawn@bighugh.com>
2010-03-24 11:43:34 -0400

    Start swank if the environment variable is set. Added a swank script for Windows.

--- script/repl.bat
+++ script/repl.bat
@@ -7,4 +7,8 @@ for /R ./lib %%a in (*.jar) do (
 set CLASSPATH=!CLASSPATH!"
 set CLASSPATH=%CLASSPATH%;src;test;config;data
 echo CLASSPATH=%CLASSPATH%
-java -Xmx1G -cp %CLASSPATH% jline.ConsoleRunner clojure.main -i script/run.clj -r
+
+@rem jline breaks inferior-lisp.
+if not defined LABREPL_SWANK set JLINE=jline.ConsoleRunner
+
+java -Xmx1G -cp %CLASSPATH% %JLINE% clojure.main -i script/run.clj -r
