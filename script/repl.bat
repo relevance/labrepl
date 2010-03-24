a7085c754dbcc5eec0ae388c8ac0c5c0e40e93dc
Shawn Hoover <shawn@bighugh.com>
2010-03-24 10:39:37 -0400

    Update windows repl script to call run.clj.

--- script/repl.bat
+++ script/repl.bat
@@ -7,4 +7,4 @@ for /R ./lib %%a in (*.jar) do (
 set CLASSPATH=!CLASSPATH!"
 set CLASSPATH=%CLASSPATH%;src;test;config;data
 echo CLASSPATH=%CLASSPATH%
-java -Xmx1G -cp %CLASSPATH% jline.ConsoleRunner clojure.main -e "(use '[clojure.contrib.duck-streams :only (spit read-lines reader writer)] '[clojure.contrib def ns-utils pprint repl-utils shell-out]) (require '[clojure.contrib.str-utils2 :as s])(require 'labrepl)(labrepl/-main)" -r
+java -Xmx1G -cp %CLASSPATH% jline.ConsoleRunner clojure.main -i script/run.clj -r
