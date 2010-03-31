767ee67b5cdd913770d95af336424d203e90ca39
Shawn Hoover <shawn@bighugh.com>
2010-04-01 01:37:00 +0800

    Use the result of re-find, instead of seq-hacking the string.

--- script/run.clj
+++ script/run.clj
@@ -1,8 +1,8 @@
 (when-let [run-swank (System/getenv "LABREPL_SWANK")]
   (println "Starting swank...")
   ;; Drop the enclosing double quotes from the environment variable and eval it.
-  (load-string (if (re-find #"^\".*\"$" run-swank)
-                 (->> run-swank (drop 1) (butlast) (apply str))
+  (load-string (if-let [found (re-find #"^\"(.*)\"$" run-swank)]
+                 (second found)
                  run-swank)))
 
 (require 'labrepl)
