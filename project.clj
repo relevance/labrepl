275bbf9221e98b70e16d782a6ff9d217d655a743
Aaron Bedra <aaron@aaronbedra.com>
2010-10-15 09:31:41 -0400

    moving back to clojure-1.2.0 to fix a few issues

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject labrepl "0.0.1"
   :description "Clojure exercises, with integrated repl and webapp"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.4.0"]
                  [ring/ring-jetty-adapter "0.2.3"]
