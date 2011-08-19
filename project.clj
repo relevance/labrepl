c88b555335bb6991f8319237d46fa416efb50a84
Aaron Bedra <aaron@aaronbedra.com>
2011-08-19 13:05:10 -0400

    updating dependencies, switching clojure to beta1, and fixing tests

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject labrepl "0.0.2-SNAPSHOT"
   :description "Clojure exercises, with integrated repl and webapp"
-  :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.clojure/tools.logging "0.1.2"]
                  [org.clojure/data.json "0.1.0"]
-                 [ring/ring-jetty-adapter "0.3.7" :exclusions [org.clojure/clojure
-                                                               org.clojure/clojure-contrib]]
-                 [compojure "0.6.3" :exclusions [org.clojure/clojure]]
-                 [hiccup "0.3.5" :exclusions [org.clojure/clojure]]
+                 [ring/ring-jetty-adapter "0.3.11" :exclusions [org.clojure/clojure
+                                                                org.clojure/clojure-contrib]]
+                 [compojure "0.6.5" :exclusions [org.clojure/clojure]]
+                 [hiccup "0.3.6" :exclusions [org.clojure/clojure]]
                  [log4j "1.2.16" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]
-                 [swank-clojure "1.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+                 [swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]
                  [jline "0.9.94"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]])
+  :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]])
--- test/solutions/browser_mockup_test.clj
+++ test/solutions/browser_mockup_test.clj
@@ -5,5 +5,5 @@
 (deftest test-mockups
     (doseq [url ["/m1" "/m2" "/m3" "/m4"]]
     (let [resp (mockup-routes {:request-method :get :uri url})]
-      (is (= {:status 200 :headers  {"Content-Type" "text/html"}}
+      (is (= {:status 200 :headers  {"Content-Type" "text/html; charset=utf-8"}}
              (select-keys resp [:status :headers]))))))
