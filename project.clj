ec17e2ffd5da028ca362d6a618bb96993ee73773
Aaron Bedra <aaron@aaronbedra.com>
2011-10-24 10:43:09 -0400

    Updating dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
 (defproject labrepl "0.0.2-SNAPSHOT"
   :description "Clojure exercises, with integrated repl and webapp"
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [org.clojure/tools.logging "0.1.2"]
-                 [org.clojure/data.json "0.1.0"]
-                 [ring/ring-jetty-adapter "0.3.11" :exclusions [org.clojure/clojure
-                                                                org.clojure/clojure-contrib]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/data.json "0.1.2"]
+                 [ring/ring-jetty-adapter "1.0.0-RC1" :exclusions [org.clojure/clojure
+                                                                   org.clojure/clojure-contrib]]
                  [compojure "0.6.5" :exclusions [org.clojure/clojure]]
-                 [hiccup "0.3.6" :exclusions [org.clojure/clojure]]
+                 [hiccup "0.3.7" :exclusions [org.clojure/clojure]]
                  [log4j "1.2.16" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]
-                 [swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]
                  [jline "0.9.94"]]
   :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]])
